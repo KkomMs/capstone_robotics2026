940f8fe164c9f9bd69210dc89aec84cef4e21e95
KkomMs <cdioe88@hanyang.ac.kr>
2026-04-16 00:53:45 +0900

    add: apply LPF to inwheel motor feedback

--- fwis_robot/src/mobile_robot_node.cpp
+++ fwis_robot/src/mobile_robot_node.cpp
@@ -119,6 +119,7 @@ private:
         odom_frame_id_ = GetParam<std::string>(*this, "odom_frame_id");
         loop_hz_       = GetParam<double>(*this, "control_rate");
         publish_tf_    = GetParam<bool>(*this, "publish_tf");
+        lpf_alpha_     = GetParam<double>(*this, "lpf_alpha");
 
         // --- Joint Names -------------------------------------
         steer_joint_names_[0] = GetParam<std::string>(*this, "front_left_steer_joint");
@@ -221,6 +222,7 @@ private:
         current_states_[2].steering_ang = static_cast<double>(msg->data[1]);    // [RL]
         current_states_[3].steering_ang = static_cast<double>(msg->data[0]);    // [RR]
     }
+    
     // ==========================================================
     //  Inwheel motor feedback 콜백 [m/s]
     // ==========================================================
@@ -230,8 +232,15 @@ private:
             RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 3000, "[InwheelFront] /wheel_vel/front data size < 2");
             return;
         }
-        current_states_[0].wheel_vel = static_cast<double>(msg->data[0]);       // [FL]
-        current_states_[1].wheel_vel = static_cast<double>(msg->data[1]);       // [FR]
+        // front 모터 feedback에 LPF 적용
+        for (int i = 0; i < 2; i++) {
+            if (std::abs(msg->data[i]) < 0.001) {
+                filtered_vel_[i] = 0.0;
+            } else {
+                filtered_vel_[i] = (lpf_alpha_ * msg->data[i]) + ((1.0 - lpf_alpha_) * filtered_vel_[i]);
+            }
+            current_states_[i].wheel_vel = filtered_vel_[i];
+        }
     }
     void HandleInwheelRear(const std_msgs::msg::Float32MultiArray::SharedPtr msg)
     {
@@ -239,8 +248,15 @@ private:
             RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 3000, "[InwheelFront] /wheel_vel/rear data size < 2");
             return;
         }
-        current_states_[2].wheel_vel = static_cast<double>(msg->data[0]);       // [RL]
-        current_states_[3].wheel_vel = static_cast<double>(msg->data[1]);       // [RR]
+        // rear 모터 feedback에 LPF 적용
+        for (int i = 0; i < 2; i++) {
+            if (std::abs(msg->data[i]) < 0.001) {
+                filtered_vel_[i+2] = 0.0;
+            } else {
+                filtered_vel_[i+2] = (lpf_alpha_ * msg->data[i]) + ((1.0 - lpf_alpha_) * filtered_vel_[i+2]);
+            }
+            current_states_[i+2].wheel_vel = filtered_vel_[i+2];
+        }
     }
 
     // ==========================================================
@@ -418,6 +434,8 @@ private:
     std::unordered_map<std::string, int> inwheel_name_to_idx_;
     double      loop_hz_;
     bool        publish_tf_;
+    double      lpf_alpha_;
+    std::array<double, 4> filtered_vel_ = {0.0, 0.0, 0.0, 0.0};
 
     // 제어 객체
     Controller controller_;
